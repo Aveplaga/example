91071fce4b822d8e9a55b78d2d1858af0988c739
Vladislav Hmelev <vladislavhmelev9@gmail.com>
2025-09-22 16:02:34 +0900

    header

--- skeleton/sites/verstka/include/header.php
+++ skeleton/sites/verstka/include/header.php
@@ -65,14 +65,26 @@ $mustache = new Mustache_Engine([
                         <li class="header item">
                             <a href="#contact" class="header-link">contact</a>
                         </li>
+                    </ul>
+                    <ul class="header-list header-cart">
                         <li class="header-item header-cart">
                             <a href="" class="header-link">
-                                <svg width="20" height="20" viewBox="0 0 20 20" fill="#3c3c3c;"
+                                <svg width="16" height="16" viewBox="0 0 20 20" fill="#3c3c3c;"
                                     xmlns="http://www.w3.org/2000/svg">
                                     <path fill-rule="evenodd" clip-rule="evenodd"
                                         d="M10.0102 4.64303L9.10439 3.7372C7.31449 1.9473 4.41248 1.9473 2.62258 3.7372C0.832676 5.52711 0.832676 8.42911 2.62258 10.219L9.98827 17.5847L9.98977 17.5832L10.0117 17.6052L17.3774 10.2395C19.1673 8.44956 19.1673 5.54755 17.3774 3.75765C15.5875 1.96775 12.6855 1.96775 10.8956 3.75765L10.0102 4.64303ZM9.98977 15.2262L14.1033 11.1127L15.3123 9.94513L15.3135 9.94633L16.1989 9.06095C17.3379 7.92192 17.3379 6.07519 16.1989 4.93616C15.0599 3.79713 13.2131 3.79713 12.0741 4.93616L10.0111 6.99916L10.0051 6.99312L10.0042 6.99402L7.92588 4.91571C6.78685 3.77668 4.94012 3.77668 3.80109 4.91571C2.66206 6.05474 2.66206 7.90147 3.80109 9.0405L5.91728 11.1567L5.91878 11.1552L9.98977 15.2262Z" />
                                 </svg>
                                 <span class="cart-count">0</span>
+                            </a>
+                        </li>
+                        <li class="header-item header-profile">
+                            <a href="#profile" class="header-link">
+                                <svg width="16" height="16" viewBox="0 0 24 24" fill="#3c3c3c" xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M12 12c2.761 0 5-2.239 5-5s-2.239-5-5-5-5 
+                                            2.239-5 5 2.239 5 5 5zm0 2c-3.866 0-7 
+                                            3.134-7 7h2c0-2.761 2.239-5 5-5s5 2.239 
+                                            5 5h2c0-3.866-3.134-7-7-7z" />
+                                </svg>
                             </a>
                         </li>
                     </ul>
